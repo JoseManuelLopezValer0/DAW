dbb781bc16e94f223d24d1775c52f2a90d517054
Jose Manuel Lopez Valero <josemanuellopezgamer@gmail.com>
2022-10-06 14:42:42 +0200

    .

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$aasignaturas = array(
+    "DWES" => array("titulo" => "DWES", "Enlace" => "<a href='DAW/DWES'>DWES</a>",),
+);
+
+foreach ($aEjercicios as $i => $value1) {
+    echo $i;
+    echo "<p>";
+    foreach ($aEjercicios[$i] as $j => $value2) {
+        
+        echo "<p>";
+    }
+    echo "<br>";
+};
+echo"enlace";
