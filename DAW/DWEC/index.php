ea6793a6ffe9c5b800ca5a923639bef2a1f0322b
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2022-10-21 12:37:13 +0200

    .

--- DAW/DWEC/index.php
+++ DAW/DWEC/index.php
@@ -1,27 +1,27 @@
 <?php
 $aEjercicios = array(
     "ejercicio 1" => array(
-        array("titulo" => "ejercicio 1", "Enlace" => "<a href='DWEC/ejercicio 1/ '>"),
-        array("titulo" => "ejercicio 2", "Enlace" => "<a href='DWEC/ejercicio 1/3º/ '>"),
-        array("titulo" => "ejercicio 3", "Enlace" => "<a href='DWEC/ejercicio 1/2º/ '>"),
-        array("titulo" => "ejercicio 4", "Enlace" => "<a href='DWEC/ejercicio 1/1º/ '>"),
+        array("titulo" => "ejercicio 1", "Enlace" => "<a href='ejercicio 1/ '>"),
+        array("titulo" => "ejercicio 2", "Enlace" => "<a href='ejercicio 1/3º/ '>"),
+        array("titulo" => "ejercicio 3", "Enlace" => "<a href='ejercicio 1/2º/ '>"),
+        array("titulo" => "ejercicio 4", "Enlace" => "<a href='ejercicio 1/1º/ '>"),
     ),
     "ejercicio 2" => array(
-        array("titulo" => "ejercicio 2", "Enlace" => "<a href='DWEC/ejercicio 2/ '>"),
+        array("titulo" => "ejercicio 2", "Enlace" => "<a href='ejercicio 2/ '>"),
     ),
     "sobre-mi-JoseManuelLopezValer0" => array(
-        array("titulo" => "sobre-mi-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/sobre-mi-JoseManuelLopezValer0/ '>"),
+        array("titulo" => "sobre-mi-JoseManuelLopezValer0", "Enlace" => "<a href='sobre-mi-JoseManuelLopezValer0/ '>"),
     ),
     "UD 1" => array(
-        array("titulo" => "Actividad 1", "Enlace" => "<a href='DWEC/UD 1/Actividad 1 '>"),
-        array("titulo" => "ud1-a3-etiqueta-script-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 1/ud1-a3-etiqueta-script-JoseManuelLopezValer0 '>"),
+        array("titulo" => "Actividad 1", "Enlace" => "<a href='UD 1/Actividad 1 '>"),
+        array("titulo" => "ud1-a3-etiqueta-script-JoseManuelLopezValer0", "Enlace" => "<a href='UD 1/ud1-a3-etiqueta-script-JoseManuelLopezValer0 '>"),
     ),
     "UD 2" => array(
-        array("titulo" => "ej1-17-introduccion-a-javascritp-javier-eguiluz-perez-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 2/ej1-17-introduccion-a-javascritp-javier-eguiluz-perez-JoseManuelLopezValer0 '>"),
-        array("titulo" => "tarea-2-5-tipos-de-datos-primitivos-y-objetos-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 2/tarea-2-5-tipos-de-datos-primitivos-y-objetos-JoseManuelLopezValer0'>"),
-        array("titulo" => "tarea-2-7-organismos-y-estandares-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 2/tarea-2-7-organismos-y-estandares-JoseManuelLopezValer0'>"),
-        array("titulo" => "tarea-2-8-ambitos-y-closures-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 2/tarea-2-8-ambitos-y-closures-JoseManuelLopezValer0'>"),
-        array("titulo" => "tarea-2-9-los-tres-puntitos-JoseManuelLopezValer0", "Enlace" => "<a href='DWEC/UD 2/tarea-2-9-los-tres-puntitos-JoseManuelLopezValer0'>"),
+        array("titulo" => "ej1-17-introduccion-a-javascritp-javier-eguiluz-perez-JoseManuelLopezValer0", "Enlace" => "<a href='UD 2/ej1-17-introduccion-a-javascritp-javier-eguiluz-perez-JoseManuelLopezValer0 '>"),
+        array("titulo" => "tarea-2-5-tipos-de-datos-primitivos-y-objetos-JoseManuelLopezValer0", "Enlace" => "<a href='UD 2/tarea-2-5-tipos-de-datos-primitivos-y-objetos-JoseManuelLopezValer0'>"),
+        array("titulo" => "tarea-2-7-organismos-y-estandares-JoseManuelLopezValer0", "Enlace" => "<a href='UD 2/tarea-2-7-organismos-y-estandares-JoseManuelLopezValer0'>"),
+        array("titulo" => "tarea-2-8-ambitos-y-closures-JoseManuelLopezValer0", "Enlace" => "<a href='UD 2/tarea-2-8-ambitos-y-closures-JoseManuelLopezValer0'>"),
+        array("titulo" => "tarea-2-9-los-tres-puntitos-JoseManuelLopezValer0", "Enlace" => "<a href='UD 2/tarea-2-9-los-tres-puntitos-JoseManuelLopezValer0'>"),
     ),
 
 
