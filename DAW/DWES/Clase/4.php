6b36c1a09f701925587a8c48acc788478b6e94e6
JoseManuelLopezValer0 <a22lovajo@iesgrancapitan.org>
2022-10-22 21:17:15 +0200

    .

---
+++ DAW/DWES/Clase/4.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Documentacion:
+ * 
+ * Creamos un bucle para mostrar una tabla de cololres con intervalos definidos
+ * En mi caso seran de 15 por cada color (17 colores por cada valor "r", "g" o "b")
+ *  @author:lopezValero
+ */
+
+    $i=15;
+    $r=0;
+    $g=0;
+    $b=0;
+    echo "a</p>";
+//  for ($r=0; $r < 255 ; $r+$i) { 
+//      for ($g=0; $g < 255 ; $g+$i) { 
+//          for ($b=0; $b < 255 ; $b+$i) { 
+//             $color="$r$g$b";
+//             // echo "$color</p>";
+//             echo "a</p>";
+//         }
+//      }
+//  }
