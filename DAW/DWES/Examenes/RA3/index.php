31b88b3087cc2055fd635b909b9d77b07e907095
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2022-10-31 18:44:47 +0100

    Examen v2

--- DAW/DWES/Examenes/RA3/index.php
+++ DAW/DWES/Examenes/RA3/index.php
@@ -11,71 +11,35 @@
 include 'lib/funciones.php';
 include 'config/config.php';
 
-if (!isset($_POST['enviar'])) {
-?>
-    <form action='index.php' method='post'>
-        Equipo: <select name='equipo'>
-            <?php
-            foreach (Tarifa as $key => $value) {
-                echo "<option value='$key'>$key</option>";
-            }
-            ?>
-        </select>
-        <br>
-        Localidad
-        <input type='radio' name='zona' value='A'>A
-        <input type='radio' name='zona' value='B'>B
-        <input type='radio' name='zona' value='C'>C
-        <input type='radio' name='zona' value='D'>D
-        <br>
-        <table border='1'>
-            <?php
-            $localidadesSocios = localidadesOcupadas();
-            $i = 1;
-            for ($f = 1; $f <= 10; $f++) {
-                echo "<tr>";
-                for ($c = 1; $c <= 10; $c++) {
-                    if (in_array($i, $localidadesSocios)) {
-                        echo "<td style='background-color:gray'>$i</td>";
-                    } else {
-                        echo "<td>";
-                        echo "<input type='checkbox' name='localidades[]' value='$i'> $i";
-                        echo "</td>";
-                    }
-                    $i++;
-                }
-                "</tr>";
-            }
-            ?>
-        </table>
+$procesarFormulario = false;
+$localidadesSocios;
+$localidadesSeleccionadas;
+$zona;
+$equipo;
+$precio;
 
-
-        <input type='submit' name='enviar' value='Enviar'>
-
-    </form>
-<?php
-
-} else {
-
-    if (isset($_POST['enviar']) && !isset($_POST['localidades'])) {
-        echo "<p style='color:red'>Debe seleccionar al menos una localidad</p>";
-        echo "<a href='index.php'>Volver</a>";
-    } else if (!isset($_POST['zona'])) {
-        echo "<p style='color:red'>Debe seleccionar una zona</p>";
-        echo "<a href='index.php'>Volver</a>";
-    } else {
-        $localidadesSocios = localidadesOcupadas();
-        $localidadesSeleccionadas = $_POST['localidades'];
-        $zona = $_POST['zona'];
-        $equipo = $_POST['equipo'];
-        $precio = Tarifa[$equipo][$zona];
-        $total = count($localidadesSeleccionadas) * $precio;
-        echo "<p>Localidades seleccionadas: " . count($localidadesSeleccionadas) . "</p>";
-        echo "<p>Precio: $precio €</p>";
-        echo "<p>Total: $total €</p>";
-        echo "<input type='button' value='Volver' onclick='history.back()'>";
-    }
+if (isset($_POST['enviar']) && !isset($_POST['localidades'])) {
+    $procesarFormulario = false;
+    echo "<p style='color:red'>Debe seleccionar al menos una localidad</p>";
+    echo "<a href='index.php'>Volver</a>";
+} else if (isset($_POST['enviar']) && !isset($_POST['zona'])) {
+    $procesarFormulario = false;
+    echo "<p style='color:red'>Debe seleccionar una zona</p>";
+    echo "<a href='index.php'>Volver</a>";
+} else if (isset($_POST['enviar'])) {
+    $procesarFormulario = true;
+    $localidadesSocios = localidadesOcupadas();
+    $localidadesSeleccionadas = $_POST['localidades'];
+    $zona = $_POST['zona'];
+    $equipo = $_POST['equipo'];
+    $precio = Tarifa[$equipo][$zona];
+    $total = count($localidadesSeleccionadas) * $precio;
+    echo "<p>Localidades seleccionadas: " . count($localidadesSeleccionadas) . "</p>";
+    echo "<p>Precio: $precio €</p>";
+    echo "<p>Total: $total €</p>";
+    echo "<input type='button' value='Volver' onclick='history.back()'>";
 }
+
 
 ?>
 <!DOCTYPE html>
@@ -88,8 +52,33 @@ if (!isset($_POST['enviar'])) {
 </head>
 
 <body>
+    <form action='index.php' method='post'>
+        Equipo: <select name='equipo'>
+            <?php
+            foreach (Tarifa as $key => $value) {
+                echo "<option value='$key'>$key</option>";
+            }
+            ?>
+        </select>
+        <br>
+        Zona
+        <input type='radio' name='zona' value='A'>A
+        <input type='radio' name='zona' value='B'>B
+        <input type='radio' name='zona' value='C'>C
+        <input type='radio' name='zona' value='D'>D
+        <br>
+        <table border='1'>
+            <?php
+            $localidadesSocios = localidadesOcupadas();
+            mostrarLocalidades($localidadesSocios);
+            ?>
+            
+        </table>
 
 
+        <input type='submit' name='enviar' value='Enviar'>
+
+    </form>
 </body>
 
 </html>
--- DAW/DWES/Examenes/RA3/lib/funciones.php
+++ DAW/DWES/Examenes/RA3/lib/funciones.php
@@ -9,7 +9,6 @@
  * 
  */
 
-// proceso aleatorio que carga en un array las localidades ocupadas por los socios habiendo 4 zonas con 100 localidades cada una y el numero de asociados ocupados es definido en config.php como NumeroAbonos
 function localidadesOcupadas()
 {
     $localidadesSocios = array();
@@ -17,4 +16,79 @@ function localidadesOcupadas()
         $localidadesSocios[] = rand(1, 400);
     }
     return $localidadesSocios;
+}
+function mostrarLocalidades($localidadesSocios)
+{
+    echo "<br>";
+    switch 
+    (isset($_POST['zona'])) {
+        case 'A':
+            $i=1;
+            for ($f = 1; $f <= 10; $f++) {
+                echo "<tr>";
+                for ($c = 1; $c <= 10; $c++) {
+                    if (in_array($i, $localidadesSocios)) {
+                        echo "<td style='background-color:gray'>$i</td>";
+                    } else {
+                        echo "<td>";
+                        echo "<input type='checkbox' name='localidades[]' value='$i'> $i";
+                        echo "</td>";
+                    }
+                    $i++;
+                }
+                "</tr>";
+            }
+            break;
+        case 'B':
+            $i=101;
+            for ($f = 1; $f <= 10; $f++) {
+                echo "<tr>";
+                for ($c = 1; $c <= 10; $c++) {
+                    if (in_array($i, $localidadesSocios)) {
+                        echo "<td style='background-color:gray'>$i</td>";
+                    } else {
+                        echo "<td>";
+                        echo "<input type='checkbox' name='localidades[]' value='$i'> $i";
+                        echo "</td>";
+                    }
+                    $i++;
+                }
+                "</tr>";
+            }
+            break;
+        case 'C':
+            $i=201;
+            for ($f = 1; $f <= 10; $f++) {
+                echo "<tr>";
+                for ($c = 1; $c <= 10; $c++) {
+                    if (in_array($i, $localidadesSocios)) {
+                        echo "<td style='background-color:gray'>$i</td>";
+                    } else {
+                        echo "<td>";
+                        echo "<input type='checkbox' name='localidades[]' value='$i'> $i";
+                        echo "</td>";
+                    }
+                    $i++;
+                }
+                "</tr>";
+            }
+            break;
+        case 'D':
+            $i=301;
+            for ($f = 1; $f <= 10; $f++) {
+                echo "<tr>";
+                for ($c = 1; $c <= 10; $c++) {
+                    if (in_array($i, $localidadesSocios)) {
+                        echo "<td style='background-color:gray'>$i</td>";
+                    } else {
+                        echo "<td>";
+                        echo "<input type='checkbox' name='localidades[]' value='$i'> $i";
+                        echo "</td>";
+                    }
+                    $i++;
+                }
+                "</tr>";
+            }
+            break;
+    }
 }
