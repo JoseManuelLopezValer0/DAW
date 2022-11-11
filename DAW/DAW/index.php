d8f2ebf5b814dbb893138728140a8fefe35be567
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2022-11-11 08:45:11 +0100

    .

---
+++ DAW/DAW/index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Desarrollo interfaces web - Jose Manuel Lopez Valero</title>
+    <link rel="stylesheet" href="css/style.css">
+    </head>
+
+<body>
+    <header class="super-cabecera">
+        <nav>
+            <ul>
+                <li><a href="../../">
+                        <i class="fa fa-fw fa-home fa-5x"></i>
+                        <span class="borde"></span>
+                        <span class="texto">VOLVER</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <hr>
+    <?php
+include "../../DAW/config/config.php";
+obtener_estructura_directorios(".");
+    ?>
+</body>
+
+</html>
