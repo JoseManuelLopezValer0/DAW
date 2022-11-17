bf454df403f2a375e56463e268c38ce20405ea9a
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2022-11-17 10:14:42 +0100

    correccion UL

--- lib/cabecera.php
+++ lib/cabecera.php
@@ -1,5 +1,5 @@
 <nav>
-    <ul>
+    <ul class="cabecera-ul">
         <li><a href="/">Inicio</a></li>
         <li><a href="https://github.com/JoseManuelLopezValer0/DAW">Repositorio</a></li>
         <li><a href="mailto:lovajomafidianagmail.com">Contacto</a></li>
