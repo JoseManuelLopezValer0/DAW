ab16bd57cfa3aab88f171d271a5916680ec6d108
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2022-12-09 11:12:18 +0100

    .

--- DAW/DWES/UD6/Ejercicio-Clase/test/test_equipos.php
+++ DAW/DWES/UD6/Ejercicio-Clase/test/test_equipos.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../vendor/autoload.php');
 use Dotenv\Dotenv;
-use App\Models\Equipo;
+use App\Models\Equipos;
 
 session_start();
 ini_set('display_errors',1);
@@ -10,8 +10,12 @@ error_reporting(E_ALL);
 
 $dotenv = Dotenv::createImmutable(__DIR__.'/..');
 $dotenv->load();
-define('DBHOST', $\_ENV['DB_HOST']);
-define('DBUSER', $\_ENV['DB_USER']);
-define('DBPASS', $\_ENV['DB_PASS']);
-define('DBNAME', $\_ENV['DB_NAME']);
-define('DBPORT', $\_ENV['DB_PORT']);
+define('DBHOST', $_ENV['DB_HOST']);
+define('DBUSER', $_ENV['DB_USER']);
+define('DBPASS', $_ENV['DB_PASS']);
+define('DBNAME', $_ENV['DB_NAME']);
+define('DBPORT', $_ENV['DB_PORT']);
+
+
+$equipo = new Equipos();
+$equipo->setNombre('Barcelona');
