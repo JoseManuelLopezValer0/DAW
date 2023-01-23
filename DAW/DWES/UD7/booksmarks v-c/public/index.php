c71826f0ffc1e7a8c79e1c59d7b4843e089fc0d8
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2023-01-23 12:42:58 +0100

    UD8,7

--- DAW/DWES/UD7/booksmarks v-c/public/index.php
+++ DAW/DWES/UD7/booksmarks v-c/public/index.php
@@ -16,23 +16,36 @@ $db = conectaDB();
 session_start();
 $msgError = "";
 
-$_SESSION['user'] = ' ';
+$_SESSION['user'] = 'invitado';
+
+$db = conectaDB();
+const errores = 0;
 
 if (isset($_POST['login'])) {
     $user = $_POST['user'];
     $password = $_POST['password'];
-    $sql = "SELECT * FROM users WHERE user = :user AND psw = :password";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':user', $user);
-    $stmt->bindParam(':password', $password);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $result = login($db, $user, $password);
     if ($result) {
         $_SESSION['user'] = $user;
-        header('Location: index.php');
+        $_SESSION['errores'] = 0;
     } else {
-        $msgError = "Usuario o contraseña incorrectos";
+        $_SESSION['errores']++;
+        if ($_SESSION['errores'] == 3) {
+            bloquear($db, $user);
+            $msgError = "Usuario bloqueado";
+        } else {
+            $msgError = "Usuario o contraseña incorrectos";
+        }
     }
+}
+
+function login($db, $user, $password)
+{
+    $sql = "SELECT * FROM usuarios WHERE user = :user AND psw = :password";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array(":user" => $user, ":password" => $password));
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
 }
 
 ?>
@@ -47,43 +60,60 @@ if (isset($_POST['login'])) {
     <h1>Bookmarks</h1>
     <div>
         <?php
-        if ($_SESSION['user'] == ' ') {
-            echo "<form action='index.php' method='post'>";
-            include('lib/login_form.html');
-        } elseif ($_SESSION['user'] != 'invitado') {
-            if ($_SESSION['user'] == 'admin') {
-                echo 'usuario : ' . $_SESSION['user'] . ' <a href="logout.php">Cerrar sesión</a> <a href="desbloqueo.php">Desbloquear usuario</a> añadir <a href="anadir.php">añadir</a> eliminar <a href="eliminar.php">eliminar</a>';
-                $sql = "SELECT * FROM bookmarks WHERE user = admin";
-                $stmt = $db->prepare($sql);
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo "<br>";
-                if ($result) {
-                    echo "<table border='1'>";
-                    echo "<tr><th>URL</th><th>Descripción</th></tr>";
-                    foreach ($result as $row) {
-                        echo "<tr><td><a href='" . $row['bm_url'] . "'>" . $row['bm_url'] . "</a></td><td>" . $row['description'] . "</td></tr>";
+        if (isset($_SESSION['user'])) {
+            if ($_SESSION['user'] == 'invitado') {
+                echo '<form action="index.php" method="post">
+                <label for="user">Usuario</label>
+                <input type="text" name="user" id="user">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="password">
+                <input type="submit" name="login" value="Login">
+                </form>';
+                echo $msgError;
+            } elseif ($_SESSION['user'] != 'invitado') {
+                if ($_SESSION['user'] == 'admin') {
+                    echo 'usuario : ' . $_SESSION['user'] . ' <a href="logout.php">Cerrar sesión</a> <a href="desbloqueo.php">Desbloquear usuario</a> añadir <a href="anadir.php">añadir</a> eliminar <a href="eliminar.php">eliminar</a>';
+                    $sql = "SELECT * FROM bookmarks WHERE user = admin";
+                    $stmt = $db->prepare($sql);
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo "<br>";
+                    if ($result) {
+                        echo "<table border='1'>";
+                        echo "<tr><th>URL</th><th>Descripción</th></tr>";
+                        foreach ($result as $row) {
+                            echo "<tr><td><a href='" . $row['bm_url'] . "'>" . $row['bm_url'] . "</a></td><td>" . $row['description'] . "</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "No hay bookmarks";
                     }
-                    echo "</table>";
                 } else {
-                    echo "No hay bookmarks";
-                }
-            } else {
-                echo 'usuario : ' . $_SESSION['user'] . ' <a href="logout.php">Cerrar sesión</a> añadir <a href="anadir.php">añadir</a> eliminar <a href="eliminar.php">eliminar</a>';
-                $sql = "SELECT * FROM bookmarks WHERE user = :user";
-                $stmt = $db->prepare($sql);
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo "<br>";
-                if ($result) {
-                    echo "<table border='1'>";
-                    echo "<tr><th>URL</th><th>Descripción</th></tr>";
-                    foreach ($result as $row) {
-                        echo "<tr><td><a href='" . $row['bm_url'] . "'>" . $row['bm_url'] . "</a></td><td>" . $row['description'] . "</td></tr>";
+                    echo 'usuario : ' . $_SESSION['user'] . ' <a href="logout.php">Cerrar sesión</a> añadir <a href="anadir.php">añadir</a> eliminar <a href="eliminar.php">eliminar</a>';
+                    $sql = "SELECT * FROM bookmarks WHERE user = :user";
+                    $stmt = $db->prepare($sql);
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo "<br>";
+                    if ($result) {
+                        echo "<table border='1'>";
+                        echo "<tr><th>URL</th><th>Descripción</th></tr>";
+                        foreach ($result as $row) {
+                            echo "<tr><td><a href='" . $row['bm_url'] . "'>" . $row['bm_url'] . "</a></td><td>" . $row['description'] . "</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "No hay bookmarks";
                     }
-                    echo "</table>";
-                } else {
-                    echo "No hay bookmarks";
                 }
             }
+        } else {
+            echo '<form action="index.php" method="post">
+            <label for="user">Usuario</label>
+            <input type="text" name="user" id="user">
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="password">
+            <input type="submit" name="login" value="Login">
+            </form>';
+            echo $msgError;
         }
         ?>
     </div>
