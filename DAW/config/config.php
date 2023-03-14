6344a9858e6cb0cdbe9822311dd9982db47d2bea
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2023-03-14 08:52:21 +0100

    extensiones

--- DAW/config/config.php
+++ DAW/config/config.php
@@ -1,60 +1,60 @@
 <?php
 function obtener_estructura_directorios($ruta)
 {
-    // echo "Directorio: $ruta <BR>";
-    if (is_dir($ruta)) {
-        $gestor = opendir($ruta);
-        echo "<ul>";
+  // echo "Directorio: $ruta <BR>";
+  if (is_dir($ruta)) {
+    $gestor = opendir($ruta);
+    echo "<ul>";
 
-        while (($carpeta = readdir($gestor)) !== false) {
+    while (($carpeta = readdir($gestor)) !== false) {
 
-            $ruta_completa = $ruta . "/" . $carpeta;
+      $ruta_completa = $ruta . "/" . $carpeta;
 
-            if (
-                is_dir($ruta_completa)
-                && $carpeta != "."
-                && $carpeta != ".."
-                && $carpeta != ".git"
-                && $carpeta != ".github"
-                && $carpeta != "config"
-                && $carpeta != "css"
-                && $carpeta != "lib"
-                && $carpeta != "icon"
-                && $carpeta != "js"
-                && $carpeta != "img"
-                && $carpeta != "fonts"
-                && $carpeta != "assets"
-                && $carpeta != "data"
-                && $carpeta != "doc"
-                && $carpeta != "app"
-                && $carpeta != "App"
-                && $carpeta != "views"
-                && $carpeta != "include"
-                && $carpeta != "vendor"
-                && $carpeta != "my-app"
-                && $carpeta != "dist"
-                && $carpeta != "src"
-                && $carpeta != "node_modules"
-                && $carpeta != "public"
-                && $carpeta != "scripts"
-                && $carpeta != "script"
-                && $carpeta != "modules"
-                && $carpeta != ".vite"
-                && $carpeta != "imagenes"
-                && $carpeta != "capturas"
-                && $carpeta != "video"
-                && $carpeta != "webfonts"
-                && $carpeta != "iconos"
-                && $carpeta != ".vscode"
-            ) {
-                echo "<li class='lilista'><a class ='alista'href='$ruta_completa'><span class='spanlista'>$carpeta</span></a></li>";
-                obtener_estructura_directorios($ruta_completa);
-            } 
-        }
-
-        closedir($gestor);
-        echo "</ul>";
-    } else {
-        echo "No es una ruta de directorio valida<br/>";
+      if (
+        is_dir($ruta_completa)
+        && $carpeta != "."
+        && $carpeta != ".."
+        && $carpeta != "js"
+        && $carpeta != "css"
+        && $carpeta != "lib"
+        && $carpeta != "img"
+        && $carpeta != "doc"
+        && $carpeta != "app"
+        && $carpeta != "App"
+        && $carpeta != "src"
+        && $carpeta != ".git"
+        && $carpeta != "icon"
+        && $carpeta != "data"
+        && $carpeta != "dist"
+        && $carpeta != "views"
+        && $carpeta != "video"
+        && $carpeta != "fonts"
+        && $carpeta != ".vite"
+        && $carpeta != "config"
+        && $carpeta != "assets"
+        && $carpeta != "my-app"
+        && $carpeta != "vendor"
+        && $carpeta != "public"
+        && $carpeta != "iconos"
+        && $carpeta != "script"
+        && $carpeta != ".github"
+        && $carpeta != "include"
+        && $carpeta != "scripts"
+        && $carpeta != ".vscode"
+        && $carpeta != "modules"
+        && $carpeta != "webfonts"
+        && $carpeta != "imagenes"
+        && $carpeta != "capturas"
+        && $carpeta != "node_modules"
+      ) {
+        echo "<li class='lilista'><a class ='alista'href='$ruta_completa'><span class='spanlista'>$carpeta</span></a></li>";
+        obtener_estructura_directorios($ruta_completa);
+      }
     }
+
+    closedir($gestor);
+    echo "</ul>";
+  } else {
+    echo "No es una ruta de directorio valida<br/>";
+  }
 }
