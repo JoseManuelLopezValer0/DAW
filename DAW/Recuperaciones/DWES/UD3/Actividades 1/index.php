79258fdf602ef44dfd5cf4bd029e78f37307b007
Jose Manuel Lopez Valero <a22lovajo@iesgrancapitan.org>
2023-03-29 09:06:45 +0200

    Actividades 1

---
+++ DAW/Recuperaciones/DWES/UD3/Actividades 1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <meta name="author" content="López">
+    <title>Actividad 1</title>
+</head>
+<body>
+    <h1>Actividad 1</h1>
+    <ol>
+        <li>
+            <a href="Ejercicio 1/index.php">Ejercicio 1</a>
+        </li>
+        <li>
+            <a href="Ejercicio 2/index.php">Ejercicio 2</a>
+        </li>
+        <li>
+            <a href="Ejercicio 3/index.php">Ejercicio 3</a>
+        </li>
+        <li>
+            <a href="Ejercicio 4/index.php">Ejercicio 4</a>
+        </li>
+        <li>
+            <a href="Ejercicio 5/index.php">Ejercicio 5</a>
+        </li>
+    </ol>
+</body>
+</html>
